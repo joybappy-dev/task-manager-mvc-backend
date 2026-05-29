0a4a84d3de8f91710cfeb242f1dbda0f5bcba1af
Joy Bappy <joybappy2025@gmail.com>
2026-05-29 22:35:13 +0600

    create user model

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import User from "./models/users.model.js";
 
 dotenv.config();
 const app = express();
 
 connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello Task Manager");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ success: true, users });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running on ${process.env.PORT}`);
