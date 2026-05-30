ac96f8daf3ec44bca198e84d073705454f8e076d
Joy Bappy <joybappy2025@gmail.com>
2026-05-30 08:41:48 +0600

    create loginUser controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -23,11 +23,43 @@ export const registerUser = async (req, res) => {
       password: hashedPassword,
       role,
     };
-    // const createdUser = await User.create(newUser);
+    const createdUser = await User.create(newUser);
     res.status(200).json({
       success: true,
       message: "user created successfully",
     });
+  } catch (err) {
+    res.status(500).json({
+      message: "server error: " + err.message,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password: plainTextPassword } = req.body;
+
+    // check if user exists in db
+    const user = User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "invalid credential",
+      });
+    }
+
+    // check password
+    const isValidPassword = await bcrypt.compare(
+      plainTextPassword,
+      user.password,
+    );
+    if (!isValidPassword) {
+      return res.status(400).json({
+        message: "invalid credential",
+      });
+    }
+
+    // check login user route using thunder
+    res.send("Login successful");
   } catch (err) {
     res.status(500).json({
       message: "server error: " + err.message,
