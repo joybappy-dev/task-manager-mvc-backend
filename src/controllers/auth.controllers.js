76679fce2b5b752d3e5f5940c208c377aa91c9eb
Joy Bappy <joybappy2025@gmail.com>
2026-05-30 07:35:28 +0600

    tcreate basic register user controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,3 +1,22 @@
-export const registerUser = (req, res) => {
-  
+import User from "../models/users.model.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const newUser = {
+      name,
+      email,
+      password,
+      role,
+    };
+    const createdUser = await User.create(newUser);
+    res.status(200).json({
+      success: true,
+      message: createdUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "server error: " + err.message,
+    });
+  }
 };
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import User from "./models/users.model.js";
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 connectDB();
 
