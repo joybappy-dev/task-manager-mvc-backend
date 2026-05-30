2f25421a38aa0ffdccfe30a5663e9da7a5ffecd8
Joy Bappy <joybappy2025@gmail.com>
2026-05-30 10:16:16 +0600

    create task model

---
+++ src/models/tasks.model.js
@@ -0,0 +1,31 @@
+const { default: mongoose } = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+    priority: {
+      type: Boolean,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: true,
+  },
+);
