e8413583f7dd270f4dc66bd8d2c4804df53f2d01
Joy Bappy <joybappy2025@gmail.com>
2026-05-30 10:27:03 +0600

    define create task controller

---
+++ src/controllers/task.controllers.js
@@ -0,0 +1,24 @@
+import Task from "../models/tasks.model.js";
+
+export const createTask = async (req, res) => {
+  try {
+    const { title, description, priority, createdBy } = req.body;
+
+    const newTask = {
+      title,
+      description,
+      priority,
+      createdBy,
+    };
+
+    const createdTask = await Task.create(newTask);
+    res.status(201).json({
+      success: true,
+      createTask,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
