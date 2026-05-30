8b11836d9ee1a4104502689b5f800a092e9be65b
Joy Bappy <joybappy2025@gmail.com>
2026-05-30 08:17:58 +0600

    thash password using bcrypt and check existing user in db

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,18 +1,32 @@
 import User from "../models/users.model.js";
+import bcrypt from "bcrypt";
 
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
+
+    // check duplicate user
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "email already exists",
+      });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = {
       name,
       email,
-      password,
+      password: hashedPassword,
       role,
     };
-    const createdUser = await User.create(newUser);
+    // const createdUser = await User.create(newUser);
     res.status(200).json({
       success: true,
-      message: createdUser,
+      message: "user created successfully",
     });
   } catch (err) {
     res.status(500).json({
