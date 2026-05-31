d5900000fd430980b99bf76379595e57b934bee3
Joy Bappy <joybappy2025@gmail.com>
2026-05-31 08:15:33 +0600

    create route to get all tasks

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createTask } from "../controllers/task.controllers.js";
+import { createTask, getAllTasks } from "../controllers/task.controllers.js";
 import { veryfiyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/", veryfiyJWT, createTask);
+router.get("/", veryfiyJWT, getAllTasks);
 
 export default router;
