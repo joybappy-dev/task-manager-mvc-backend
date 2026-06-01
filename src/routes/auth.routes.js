b4c20a9692e6d766191affd35b29d31445f5d01c
Joy Bappy <joybappy2025@gmail.com>
2026-06-01 23:10:17 +0600

    use verifyJWT and verifyAdmin middlewares to get users

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/auth.controllers.js";
+import {
+  getUsers,
+  loginUser,
+  registerUser,
+} from "../controllers/auth.controllers.js";
+import { verifyAdmin, veryfiyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/users", veryfiyJWT, verifyAdmin, getUsers);
 
 export default router;
